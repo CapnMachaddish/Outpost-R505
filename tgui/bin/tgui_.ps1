bc491e3ae34dd0ce22d76041a4012a3c16974535
DragonTrance <dylan661993@gmail.com>
2021-05-24 00:02:02 -0700

    Revert "[MIRROR] Upgrade tgui dependencies, make yarn task skippable (#4578)"
    This reverts commit 9d31c8581b535d38aaed40e34f25ded3f81e1010.

--- tgui/bin/tgui_.ps1
+++ tgui/bin/tgui_.ps1
@@ -72,9 +72,8 @@ function task-clean {
   Remove-Quiet -Recurse -Force ".yarn\cache"
   Remove-Quiet -Recurse -Force ".yarn\unplugged"
   Remove-Quiet -Recurse -Force ".yarn\webpack"
-  Remove-Quiet -Force ".yarn\build-state.yml"
-  Remove-Quiet -Force ".yarn\install-state.gz"
-  Remove-Quiet -Force ".yarn\install-target"
+  Remove-Quiet -Recurse -Force ".yarn\build-state.yml"
+  Remove-Quiet -Recurse -Force ".yarn\install-state.gz"
   Remove-Quiet -Force ".pnp.js"
   ## NPM artifacts
   Get-ChildItem -Path "." -Include "node_modules" -Recurse -File:$false | Remove-Item -Recurse -Force
